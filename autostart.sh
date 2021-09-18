11ec1c03a4789dc02839f3f7f5f23bbaa3486aad
James <jamestgeiss@gmail.com>
2021-09-17 23:20:05 -0400

    Switched to slstatus.

--- autostart.sh
+++ autostart.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 
 $AUTOSTART
-
-is_mute()
-{
-  if [ amixer get Master | tail -2 | grep -q '\[on\]' ]; then
-    return "墳"
-  else
-    return "婢"
-  fi
-}
-
-while true; do
-  xsetroot -name " ﮮ $(checkupdates | wc -l) |  $(date +"%a, %d %b %I:%M %p") "
-  sleep 10s
-done &
+slstatus
